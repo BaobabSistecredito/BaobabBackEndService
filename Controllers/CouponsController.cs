c90ee6d057d133958d9575091b8292ab9eaccb4e
SkormJF <mattuniversidad@hotmail.com>
2024-05-16 18:29:30 -0500

    Se crea la funcion para mostrar los cupones deacuerdo a unos parametros de busqueda y retornar el cupon o los cupones resultantes, tambien aplica la validacion de datos y errores

--- Controllers/CouponsController.cs
+++ Controllers/CouponsController.cs
@@ -31,5 +31,42 @@ namespace BaobabBackEndSerice.Controllers
                 return StatusCode(500, new ResponseUtils<Category>(false, null, null, $"Errors: {ex.Message}"));
             }
         }
+
+        [HttpGet("{SearchType}/{Value}")]
+        public async Task<ActionResult<ResponseUtils<Coupon>>> GetCoupon(string SearchType, string Value){
+            try
+            {
+                if(!int.TryParse(SearchType, out int ParseSearchType)){
+                    return BadRequest(new ResponseUtils<Coupon>(false, null, null, "SkormJF disapproved"));
+                }
+
+                IQueryable<Coupon> CuponQuery = _context.Coupons;
+                switch (ParseSearchType){
+                    case 1:
+                        if (!int.TryParse(Value, out int couponId)) {
+                            return BadRequest(new ResponseUtils<Coupon>(false, null, null, "Invalid coupon ID"));
+                        }
+                        CuponQuery = CuponQuery.Where(c => c.Id == couponId);
+                break;
+                    case 2:
+                        CuponQuery = CuponQuery.Where(c => c.Title.StartsWith(Value));
+                        break;
+                    case 3:
+                        CuponQuery = CuponQuery.Where(c => c.CouponCode.StartsWith(Value));
+                        break;
+                    default:
+                        return BadRequest(new ResponseUtils<Coupon>(false, null, null, "SkormJF disapproved"));
+                }
+                var result = await CuponQuery.ToListAsync();
+                if (result.Count == 0) {
+                    return NotFound(new ResponseUtils<Coupon>(false, null, null, "No coupons found"));
+                }
+                return new ResponseUtils<Coupon>(true, result, null, "SkormJF approved");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ResponseUtils<Category>(false, null, null, $"Errors: {ex.Message}"));
+            }
+        }
     }
 }
