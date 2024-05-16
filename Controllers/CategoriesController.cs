799b8f17af6dd63a41e71d83e7f61a2694822485
SkormJF <mattuniversidad@hotmail.com>
2024-05-15 19:30:18 -0500

    Se elabora la funcion de validacion de traer categorias segun su estado de activas e inactivas, validacion de datos entrantes que arrogen errores desde la base de datos

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -19,10 +19,39 @@ namespace BaobabBackEndSerice.Controllers
         [HttpGet]
         public async Task<ActionResult<ResponseUtils<Category>>> GetCategory()
         {
+
             try
             {
                 var result = await _context.Categories.ToListAsync();
                 return new ResponseUtils<Category>(true, result, null, "todo oki");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ResponseUtils<Category>(false, null, null, $"Error: {ex.Message}"));
+            }
+
+        }
+
+        [HttpGet("{number}")]
+        public async Task<ActionResult<ResponseUtils<Category>>> GetCategories(string number)
+        {   
+            try
+            {   
+                
+
+                /*  */
+                if(!int.TryParse(number, out int ParseNumber)){
+                    return BadRequest(new ResponseUtils<Category>(false, null, null, "SkormJF disapproved"));
+                }
+
+                var result = await _context.Categories.ToListAsync();
+                if(ParseNumber != 1){
+                        result = result.Where(c => c.Status == "Inactivo").ToList();
+                    }
+                    else{
+                        result = result.Where(c => c.Status == "Activo").ToList();
+                    }
+                return new ResponseUtils<Category>(true, result, null, "SkormJF approved");               
             }
             catch (Exception ex)
             {
