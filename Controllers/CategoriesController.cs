8d6017f315076f7ed86c8943bd32c6e25d6b49c1
duvan <duvanmmontoya@gmail.com>
2024-05-17 15:19:31 -0500

    Se Solucionan Incovenientes referente al servicio

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -39,28 +39,57 @@ namespace BaobabBackEndSerice.Controllers
             {
                 var ValidateCategory =_context.Categories.FirstOrDefault(c =>  c.CategoryName.ToLower() ==category.CategoryName.ToLower());
 
-                if(ValidateCategory == null && (category.Status == "Inactivo" || category.Status == "Activo")){
-                    
-                    _context.Categories.Add(category);
-                    await _context.SaveChangesAsync();
-                    var Categoria = CreatedAtAction("GetCategory",new{id = category.Id},category);
-                    return new ResponseUtils<Category>(true, new List<Category> { category }, null, "Todo oki");
+                //Valida y el nombre de la categoria es null o esta vacio
+                if(string.IsNullOrWhiteSpace(category.CategoryName) || category.CategoryName == null){
+
+                    return BadRequest(new ResponseUtils<Category>(false,null, null, "el nombre es un campo obligatorio."));
+
+                }else if (ValidateCategory != null){//valida si el nombre de la categoria ya existe en la base de datos
+
+                    return BadRequest(new ResponseUtils<Category>(false,null, null, "El Nombre de la Categoria ya existe"));
+
+                }else{
+                    //valida si el estado es null o es diferente a Inactivo o Activo
+                    if(category.Status == "Inactivo" ||category.Status == "Activo"){
+
+                    }else {
+
+                        return BadRequest(new ResponseUtils<Category>(false,null, null, "El estado ingresado no es Valido"));
+                    }
+
+                    //Crear Categoria
+                    if(ValidateCategory == null && (category.Status == "Inactivo" || category.Status == "Activo")){
+
+                        //poner en minuscula los datos de la base de datos
+                        category.Status = category.Status.ToLower();
+                        category.CategoryName = category.CategoryName.ToLower();
+                        
+                        _context.Categories.Add(category);
+                        await _context.SaveChangesAsync();
+                        var Categoria = CreatedAtAction("GetCategory",new{id = category.Id},category);
+                        return new ResponseUtils<Category>(true, new List<Category> { category }, null, "Todo oki");
+                    }else{
+                        return BadRequest(new ResponseUtils<Category>(false,null, null, "Error al almacenar en la base de datos"));
+                    }
 
                 }
+            
                 
-                if(category.Status == "Inactivo" ||category.Status == "Activo"){
 
-                }else {
-                    return BadRequest(new ResponseUtils<Category>(false,null, null, "El estado ingresado no es Valido"));
-                }
 
-                if(ValidateCategory != null)
+              /*   if(ValidateCategory != null)
                 {
                     return BadRequest(new ResponseUtils<Category>(false,null, null, "El Nombre de la Categoria ya existe"));
                 }
-                else{
-                    return BadRequest(new ResponseUtils<Category>(false,null, null, "Error al almacenar en la base de datos"));
+                else if(string.IsNullOrWhiteSpace(category.CategoryName)){
+
+                    return BadRequest(new ResponseUtils<Category>(false,null, null, "el nombre es un campo obligatorio."));
+
                 }
+                else{
+
+                    return BadRequest(new ResponseUtils<Category>(false,null, null, "Error al almacenar en la base de datos"));
+                } */
             }
             catch (Exception ex)
             {
