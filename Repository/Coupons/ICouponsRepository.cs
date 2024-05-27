8dde3c40734c18f87168e9fc595727a5e4eb4132
pepenauta17 <patoasesino69@gamil.com>
2024-05-26 20:45:48 -0500

    Se actuliza el editar categoria en base a los requerimientos

--- Repository/Coupons/ICouponsRepository.cs
+++ Repository/Coupons/ICouponsRepository.cs
@@ -7,60 +7,43 @@ using BaobabBackEndService.Utils;
 
 namespace BaobabBackEndService.Repository.Coupons
 {
-    public interface ICouponsRepository
-    {
-        /*
-        Parte 4:
+  public interface ICouponsRepository
+  {
+    /*
+    Parte 4:
 
-        Wenaaaas,
+    Wenaaaas,
 
-        En esta sección, declaramos las funciones de nuestro `CouponRepository`. 
-        Es aquí donde definimos los métodos necesarios para agregar, editar o eliminar cupones.
+    En esta sección, declaramos las funciones de nuestro `CouponRepository`. 
+    Es aquí donde definimos los métodos necesarios para agregar, editar o eliminar cupones.
 
-        - Si necesitamos implementar nuevas funcionalidades, primero declaramos los métodos aquí.
-        - He comentado algunos métodos como ejemplos, para que sepan cómo deberían lucir. 
-          Si necesitan implementar alguno de estos métodos, simplemente descoméntenlos y completen la implementación.
+    - Si necesitamos implementar nuevas funcionalidades, primero declaramos los métodos aquí.
+    - He comentado algunos métodos como ejemplos, para que sepan cómo deberían lucir. 
+      Si necesitan implementar alguno de estos métodos, simplemente descoméntenlos y completen la implementación.
 
-        Por ejemplo:
-        - Agregar un cupón (`addCoupon`)
-        - Editar un cupón (`editCoupon`)
-        - Eliminar un cupón (`deleteCoupon`)
+    Por ejemplo:
+    - Agregar un cupón (`addCoupon`)
+    - Editar un cupón (`editCoupon`)
+    - Eliminar un cupón (`deleteCoupon`)
 
-        Volvemos a la ruta `Service/Coupons/CouponsRepository` para continuar con la implementación detallada de estos métodos.
-        Continuemos con el siguiente paso Alli en este archivo...
-        */
+    Volvemos a la ruta `Service/Coupons/CouponsRepository` para continuar con la implementación detallada de estos métodos.
+    Continuemos con el siguiente paso Alli en este archivo...
+    */
 
-        IEnumerable<Coupon> GetCoupons();
+    IEnumerable<Coupon> GetCoupons();
 
-        Coupon GetCoupon(string id);
+    Coupon GetCoupon(string id);
 
-        Coupon CreateCoupon(Coupon coupon);
+    Coupon CreateCoupon(Coupon coupon);
 
-        Task<Coupon> GetCouponByCouponCodeAsync(string couponCode);
-        Task<Coupon> GetCouponByTitleAsync(string title);
+    Task<Coupon> GetCouponByCouponCodeAsync(string couponCode);
+    Task<Coupon> GetCouponByTitleAsync(string title);
 
-        Task<IEnumerable<Coupon>>GetCouponByIdAsync(int couponId);
-        Task<IEnumerable<Coupon>>GetCouponByTitleSearchAsync(string value);
-        Task<IEnumerable<Coupon>>GetCouponByCouponCodeSearchAsync(string value);
-        Task<IEnumerable<Coupon>> GetCouponsAsync();
-
-
-
-
-        
-
-
-      
-        
-
-
-        
-
-        
-
-        //ResponseUtils<Coupon> UpdateCoupon(stirng id,Coupon coupon);
-
-        //ResponseUtils<Coupon> DeleteCoupon(string id); // en este caso no se elimina el coupon solo se cambia de estado
+    Task<IEnumerable<Coupon>> GetCouponByIdAsync(int couponId);
+    Task<IEnumerable<Coupon>> GetCouponByTitleSearchAsync(string value);
+    Task<IEnumerable<Coupon>> GetCouponByCouponCodeSearchAsync(string value);
+    Task<IEnumerable<Coupon>> GetCouponsAsync();
+    Task<Coupon> SearchCouponsByCategoryAsync(int categoryid);
 
     }
 }
